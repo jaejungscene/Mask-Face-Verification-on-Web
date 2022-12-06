9299a15754743196309d775113cd922ee289d150
jaejung <ljj0512@gmail.com>
2022-12-06 22:14:38 +0900

    complete threshold search code

--- train/model/__init__.py
+++ train/model/__init__.py
@@ -3,12 +3,12 @@ import torch
 from .iresnet import iresnet18
 
 def get_model(base_dir=None):
-    if base_dir:
-        model = iresnet18()
-        return model
-    else:
-        print("load model weigth..")
-        weights = torch.load(os.path.join(base_dir,"weights/baseline-arcface.pth"), map_location=torch.device("cpu"))
-        model = iresnet18()
-        model.load_state_dict(weights)
-        return model
+    # if base_dir:
+    #     model = iresnet18()
+    #     return model
+    # else:
+    print("load model weigth..")
+    weights = torch.load(os.path.join(base_dir,"weights/baseline-arcface.pth"), map_location=torch.device("cpu"))
+    model = iresnet18()
+    model.load_state_dict(weights)
+    return model
