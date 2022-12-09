5cf1399fbad12e6552961bdbaa7793a5488f28ca
MyeongQ <gmk0904@ajou.ac.kr>
2022-12-09 16:04:05 +0900

    fix get opt

--- server.py
+++ server.py
@@ -6,25 +6,30 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/register', methods=['GET'])
-def register_face():
+@app.route('/register/<newID>', methods=['GET']) # param: UID
+def register_face(newID):
     print("==== operating register.py ====")
+    uid = newID
+    print("user ID : ", newID)
     model = face_vectorization()
     model.capturing()
+    emb = [1,]
     print("==== register fininshed ====")
     return jsonify({
-        'result': "done"
+        'result': '1',
     })
 
 
-@app.route('/verify', methods=['GET'])
+@app.route('/verify', methods=['GET'])  # param: UID, emb
 def register_face():
     print("==== operating register.py ====")
     model = face_verifier()
     model.capturing()
+    emb = [1,]
     print("==== register fininshed ====")
     return jsonify({
-        'result': "done"
+        'result': 1, # 성공시
+        'emb' : emb,
     })
 
 
